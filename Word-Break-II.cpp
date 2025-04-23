42209d7e517e1989092c5765d76115e5cc311284
Rajnish kumar <121631730+rajnishkumar1906@users.noreply.github.com>
2025-04-23 22:41:21 +0530

    [140] [Time Beats: 100.00%] [Memory Beats: 72.65%] - LeetPush

---
+++ Word-Break-II.cpp
@@ -0,0 +1,28 @@
+class Solution {  
+public:  
+    bool isStringPresent(vector<string>& wordDict, string& str) {  
+        return find(wordDict.begin(), wordDict.end(), str) != wordDict.end();  
+    }  
+    
+    void solve(int index, string& s, vector<string>& wordDict,vector<string>& Answer, string temp) {  
+        if (index == s.length()) {  
+            Answer.push_back(temp);  
+            return;  
+        }  
+
+        string str = \\;  // Create a new string for the current position  
+        for (int i = index; i < s.length(); i++) {  
+            str += s[i];   
+            if (isStringPresent(wordDict, str)) {  
+                string newTemp = temp.empty() ? str : temp + \ \ + str;  
+                solve(i + 1, s, wordDict, Answer, newTemp);  
+            }  
+        }  
+    }  
+    
+    vector<string> wordBreak(string& s, vector<string>& wordDict) {  
+        vector<string> Answer;  
+        solve(0, s, wordDict, Answer, \\);   
+        return Answer;  
+    }  
+};  
