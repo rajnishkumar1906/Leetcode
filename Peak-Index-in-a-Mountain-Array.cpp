8a680ede9d0b7bff49f36ec35352a96fdfb1cba5
Rajnish kumar <121631730+rajnishkumar1906@users.noreply.github.com>
2025-04-27 00:53:50 +0530

    [852] [Time Beats: 100.00%] [Memory Beats: 48.82%] - LeetPush

---
+++ Peak-Index-in-a-Mountain-Array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        int left = 0;
+        int right = n-1;
+        while(left<=right)
+        {
+            int mid = left + (right-left)/2;
+            if(arr[mid] > arr[mid-1] && arr[mid]>arr[mid+1]) return mid;
+            else if(arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1])
+            {
+                left = mid+1;
+            }
+            else if(arr[mid]<arr[mid-1])
+            {
+                right = mid;
+            }
+        }
+        return -1;
+    }
+};
