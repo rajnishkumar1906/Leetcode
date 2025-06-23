56aee5c1e460c414664b57d4fbdf1af77bb88e7c
Rajnish kumar <121631730+rajnishkumar1906@users.noreply.github.com>
2025-06-23 11:01:57 +0530

    [503] [Time Beats: 100.00%] [Memory Beats: 82.59%] - LeetPush

--- Next-Greater-Element-II.cpp
+++ Next-Greater-Element-II.cpp
@@ -2,27 +2,18 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();
-        vector<int>NGE(n);
+        vector<int>nge(n,-1);
+        // Monotinic stack where lowest is at the top
         stack<int>st;
         for(int i=2*n-1;i>=0;i--)
         {
-            while(!st.empty() && nums[i%n]>=st.top())
+            while(!st.empty() && nums[i%n]>=st.top()) st.pop();
+            if(!st.empty() && i<n)
             {
-                st.pop();
-            }
-            if(i<n)
-            {   
-                if(!st.empty())
-                {
-                    NGE[i%n] = st.top();
-                }
-                else
-                {
-                    NGE[i%n] = -1;
-                }
+                nge[i%n] = st.top();
             }
             st.push(nums[i%n]);
         }
-        return NGE;
+        return nge;
     }
 };
